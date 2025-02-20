a68510d3cc1f684c8a4542760c25ed2032b242e0
kai47x <k.u.kietzke@gmail.com>
2025-02-20 17:46:42 +0100

    lost scripts added again

---
+++ Dimi/Script/InstallRustdesk.sh
@@ -0,0 +1,17 @@
+
+
+
+
+# SCRIPT DimiInstallRustdesk
+
+# www.github.com/zellview
+# by zellview media
+
+	Version=3
+
+	wget --no-clobber https://github.com/rustdesk/rustdesk/releases/download/1.3.7/rustdesk-1.3.7-x86_64.deb
+	apt install ./rustdesk-1.3.7-x86_64.deb -y
+
+	echo "DimiInstallRustdesk done"
+
+# END DimiInstallRustdesk.
